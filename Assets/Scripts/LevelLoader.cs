91e11a571d62fa837860a75b9dc513a7f943694d
Jett Trickster <chaunceygriffin3@gmail.com>
2023-02-05 14:03:59 -0800

    Last build
    If you wanna make any changes, I wont stop you, but we're about to run out of time

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,24 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    //void OnTriggerEnter2D(Collider2D collision) {
-    //    if (collision.CompareTag("Player")) {
-    //        SceneManager.LoadScene(2);
-    //    }
-    //}
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            LoadScene();
+            Debug.Log("Fucking hell this shit dont work!");
+        }
+    }
     public void LoadScene()
     {
         if (SceneManager.GetActiveScene().name == "Cutscene 1")
-            SceneManager.LoadScene("Level 1");
+            SceneManager.LoadScene(1);
         else if (SceneManager.GetActiveScene().name == "Level 1")
-            SceneManager.LoadScene("level 2");
+        {
+            Debug.Log("Not Cutscene 1");
+            SceneManager.LoadScene(2);
+        }
         else if (SceneManager.GetActiveScene().name == "level 2")
-            SceneManager.LoadScene("Cutscene 2");
+        {
+            Debug.Log("Not Level 1");
+            SceneManager.LoadScene(3);
+        }
         else if (SceneManager.GetActiveScene().name == "Cutscene 2")
-            SceneManager.LoadScene("Overworld2");
+        {
+            Debug.Log("Not level 2");
+            SceneManager.LoadScene(4);
+        }
         else if (SceneManager.GetActiveScene().name == "Overworld2")
-            SceneManager.LoadScene("Level 4");
-        else if (SceneManager.GetActiveScene().name == "Level 4")
-            SceneManager.LoadScene("Cutscene 1");
+        {
+            Debug.Log("Not Cutscene 2");
+            SceneManager.LoadScene(5);
+        }
+        else
+        {
+            Debug.Log("it aint none of this shit");
+        }
     }
 }
