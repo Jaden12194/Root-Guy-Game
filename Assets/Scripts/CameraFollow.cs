1262af01483a043282e25095ff90f1673dccdc7e
Jaden12194 <jadenjhas@gmail.com>
2023-02-05 03:02:49 -0800

    level 4
    added new background

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,12 +6,13 @@ public class CameraFollow : MonoBehaviour
 {
     public float FollowSpeed = 2f;
     public Transform target;
+    public float yheight;
 
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x,target.position.y, -10f);
+        Vector3 newPos = new Vector3(target.position.x,yheight, -10f);
         transform.position = Vector3.Slerp(transform.position,newPos, FollowSpeed*Time.deltaTime);
     }
 }
