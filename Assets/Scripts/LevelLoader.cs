d521246152e125799379f14bb4c3d04b7b544d81
Fawad Ahmad <fawadshah501@gmail.com>
2023-02-05 21:45:59 +0500

    Do Scene management

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,10 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.CompareTag("Player")) {
-            SceneManager.LoadScene(2);
-        }
+    //void OnTriggerEnter2D(Collider2D collision) {
+    //    if (collision.CompareTag("Player")) {
+    //        SceneManager.LoadScene(2);
+    //    }
+    //}
+    public void LoadScene()
+    {
+        if (SceneManager.GetActiveScene().name == "Cutscene 1")
+            SceneManager.LoadScene("Level 1");
+        else if (SceneManager.GetActiveScene().name == "Level 1")
+            SceneManager.LoadScene("Level 2");
+        else if (SceneManager.GetActiveScene().name == "Level 2")
+            SceneManager.LoadScene("Cutscene 2");
+        else if (SceneManager.GetActiveScene().name == "Cutscene 2")
+            SceneManager.LoadScene("Overworld2");
+        else if (SceneManager.GetActiveScene().name == "Overworld2")
+            SceneManager.LoadScene("Level 4");
     }
-
 }
